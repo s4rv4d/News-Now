c3cbc8e089eeaebedcfe94d893e06ebaa2fae31f
s4rv4d <sarvadshetty@gmail.com>
2018-03-18 21:29:10 +0530

    Initial Commit

---
+++ News Now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News Now
+//
+//  Created by Sarvad shetty on 3/18/18.
+//  Copyright © 2018 Sarvad shetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
