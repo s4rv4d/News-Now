da7887c2583d832dd163e90dd458217c196fd1fc
s4rv4d <sarvadshetty@gmail.com>
2018-03-21 15:33:32 +0530

    started with magnetic got background prob fixed

--- News Now/Controllers/CategoryController.swift
+++ News Now/Controllers/CategoryController.swift
@@ -11,15 +11,56 @@ import Firebase
 import FirebaseDatabase
 import Magnetic
 
-class CategoryController: UIViewController {
+class CategoryController: UIViewController, MagneticDelegate {
     
     //MARK:Variables
     var userUID:String?
     var categories:[String] = []
+    var magnetic:Magnetic?
+    let exampleColor = UIColor.black
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        magnetic?.magneticDelegate = self
+        magnetic?.allowsMultipleSelection = true
+        magnetic?.backgroundColor = .clear
+       // magnetic.backgroundColor = [UIColor colorWithWhite:myWhiteFloat alpha:myAlphaFloat];
     }
 
+    override func loadView() {
+        super.loadView()
+        let magneticView = MagneticView(frame: CGRect(x: 0, y: 0, width: 375, height: 450))
+        magnetic = magneticView.magnetic
+        magneticView.backgroundColor = .clear
+        self.view.addSubview(magneticView)
+        view.bringSubview(toFront: magneticView)
+        
+        let node = Node(text: "Italy", image: nil, color: .red, radius: 30)
+        magnetic?.addChild(node)
+        let node2 = Node(text: "france", image: nil, color: .red, radius: 30)
+        magnetic?.addChild(node2)
+        let node3 = Node(text: "india", image: nil, color: .red, radius: 30)
+        magnetic?.addChild(node3)
+        
+    }
+    
+    func magnetic(_ magnetic: Magnetic, didSelect node: Node) {
+        showChild()
+    }
+    
+    func magnetic(_ magnetic: Magnetic, didDeselect node: Node) {
+        print("bye")
+        showChild()
+    }
+    func showChild(){
+        guard let num = magnetic?.selectedChildren.count else{return}
+        categories = []
+        for i in 0..<num{
+            categories.append((magnetic?.selectedChildren[i].label.text!)!)
+        }
+        print(categories)
+    }
+    
 
 }
